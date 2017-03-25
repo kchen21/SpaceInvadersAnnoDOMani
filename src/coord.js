275782c1cfb96ebe0730d8c6933ef6499036e03b
Kenneth Chen <kennethchendbgt@gmail.com>
2017-03-25 19:55:01 -0400

    Create classes for objects

---
+++ src/coord.js
@@ -0,0 +1,27 @@
+class Coord {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+    this.directions = {
+      "up": [-1, 0],
+      "down": [1, 0],
+      "right": [0, 1],
+      "left": [0, -1]
+    };
+  }
+
+  shift(direction) {
+    const deltas = this.directions(direction);
+    return new Coord(this.x + deltas[0], this.y + deltas[1]);
+  }
+
+  isEqual(coord2) {
+    return (this.x === coord2.x && this.y === coord2.y);
+  }
+
+  includedIn(arr) {
+    return arr.some(this.isEqual.bind(this));
+  }
+}
+
+module.exports = Coord;
