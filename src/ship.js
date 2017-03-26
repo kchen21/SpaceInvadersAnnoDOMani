da6f5aecf5c965890fed0a37f68780d566648ae7
Kenneth Chen <kennethchendbgt@gmail.com>
2017-03-26 13:29:35 -0400

    Update Ship class

--- src/ship.js
+++ src/ship.js
@@ -1,6 +1,15 @@
+const Coord = require("./Coord.js");
+
 class Ship {
   constructor(nose) {
     this.nose = nose;
+    this.body = [];
+  }
+
+  move(direction) {
+    this.body.forEach((part) => {
+      part.shift(direction);
+    })
   }
 }
 
