0143c469b788eca5c56658f435d48ff93c36b4c2
Kenneth Chen <kennethchendbgt@gmail.com>
2017-03-25 22:05:05 -0400

    Update AlienShip class

--- src/alien_ship.js
+++ src/alien_ship.js
@@ -3,6 +3,16 @@ const Ship = require("./ship.js");
 class AlienShip extends Ship {
   constructor(nose, color) {
     this.color = color;
+    this.initializeBody(nose);
+  }
+
+  createBody() {
+    this.body = [];
+
+    this.body.push(new Coor(nose[0], nose[1]));
+    this.body.push(new Coor(nose[0] - 1, nose[1]));
+    this.body.push(new Coor(nose[0] - 1, nose[1] - 1));
+    this.body.push(new Coor(nose[0] - 1, nose[1]) + 1);
   }
 }
 
--- src/game.js
+++ src/game.js
@@ -2,7 +2,7 @@ const AlienShip = require("./alien_ship.js");
 const PlayerShip = require("./player_ship.js");
 
 class Game {
-  constructor(lives = 4) {
+  constructor(lives = 3) {
     this.lives = lives;
     this.initializeAlienShips();
     this.createPlayerShip();
@@ -15,36 +15,36 @@ class Game {
     const alienShipNoses4 = [];
     const alienShipNoses5 = [];
 
-    this.aliens = [];
+    this.aliensShips = [];
 
     alienShipNoses1.forEach((nose) => {
       let ship = new AlienShip(nose, "white");
-      this.aliens.push(ship);
+      this.alienShips.push(ship);
     });
 
     alienShipNoses2.forEach((nose) => {
       let ship = new AlienShip(nose, "yellow");
-      this.aliens.push(ship);
+      this.alienShips.push(ship);
     });
 
     alienShipNoses3.forEach((nose) => {
       let ship = new AlienShip(nose, "green");
-      this.aliens.push(ship);
+      this.alienShips.push(ship);
     });
 
     alienShipNoses4.forEach((nose) => {
       let ship = new AlienShip(nose, "blue");
-      this.aliens.push(ship);
+      this.alienShips.push(ship);
     });
 
     alienShipNoses5.forEach((nose) => {
       let ship = new AlienShip(nose, "purple");
-      this.aliens.push(ship);
+      this.alienShips.push(ship);
     });
   }
 
   createPlayerShip() {
-    if (this.lives >= 0) {
+    if (this.lives > 0) {
       return new PlayerShip([27, 13]);
     }
   }
