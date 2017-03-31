7b2551f368713a1e900638339f098b8893af8970
Kenneth Chen <kennethchendbgt@gmail.com>
2017-03-31 16:00:24 -0400

    Update Shield class

--- src/shield.js
+++ src/shield.js
@@ -1,5 +1,17 @@
 class Shield {
+  constructor(boss) {
+    this.boss = boss;
+    this.parts = [];
+    this.renderParts();
+  }
 
+  renderParts() {
+    this.parts.push(new Coord(boss[0], boss[1]));
+    this.parts.push(new Coord(boss[0] + 1, boss[1] - 1));
+    this.parts.push(new Coord(boss[0] + 2, boss[1] - 2));
+    this.parts.push(new Coord(boss[0] + 1, boss[1] + 1));
+    this.parts.push(new Coord(boss[0] + 2, boss[1] + 2));
+  }
 }
 
 module.exports = Shield;
